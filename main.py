b161eef1bf6c4ddca48c1ef881d6adb375d7ee09
Alex <vandropten7@gmail.com>
2020-07-30 18:16:32 +0100

    v1.1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import sys
 import re
 import urllib.request
@@ -19,12 +17,17 @@ class UrlHTMLParser(HTMLParser):
         if tag == 'a':
             for (name, value) in attrs:
                 if name == 'href':
-                    self.urlQueue.append(str(value))
+                    # the parser does not work if the href
+                    # uses single quotations, so I forced it:
+                    if value[:2] == "\\'" and value[-2:] == "\\'":
+                        self.urlQueue.append(value[2: -2])
+                    else:
+                        self.urlQueue.append(str(value))
 
 
 class UrlHTMLParser2(HTMLParser):
 
-    """This implementation also looks for relative urls"""
+    """This implementation looks for relative URLs, too"""
 
     def __init__(self, rootUrl):
         HTMLParser.__init__(self)
@@ -39,15 +42,21 @@ class UrlHTMLParser2(HTMLParser):
                 if name == 'href':
                     # relative url
                     if not re.match('https?:\/\/.*', value):
-                        self.urlQueue.append(self.baseUrl + str(value))
+                        if self.baseUrl[-1] == '/':
+                            aux = self.baseUrl[:-1]
+                            self.baseUrl = aux
                         # the parser does not work if the href
-                        # uses single quotations, so it forces it:
+                        # uses single quotations, so I forced it:
                         if value[:2] == "\\'" and value[-2:] == "\\'":
-                            self.urlQueue.append(self.baseUrl + value[2:-2])
+                            self.urlQueue.append(self.baseUrl + '/' + value[2:-2])
+                        else:
+                            self.urlQueue.append(self.baseUrl + '/' + str(value))
                     # absolute url
                     else:
-                        self.baseUrl = str(value)
-                        self.urlQueue.append(str(value))
+                        if value[:2] == "\\'" and value[-2:] == "\\'":
+                            self.urlQueue.append(value[2:-2])
+                        else:
+                            self.urlQueue.append(str(value))
 
 
 def printUrls(urlNo, parserVersion):
@@ -62,11 +71,11 @@ def printUrls(urlNo, parserVersion):
         try:
             content = str(urllib.request.urlopen(currentUrl).read())
             if currentUrl not in parser.crawledUrls and currentUrl + '/' not in parser.crawledUrls:
+                parser.baseUrl = currentUrl
                 parser.feed(content)
                 parser.crawledUrls.add(currentUrl)
                 print(currentUrl)
         except:
             pass
-
 
 printUrls(100, 1)
